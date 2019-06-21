027b1bc924d3a8a750c1aabe130aab1dd881319f
Jack <l644081641@gmail.com>
2019-06-21 17:19:14 +0800

    构建我的页面，答题页面搁置

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -1,64 +1,80 @@
+var db = wx.cloud.database();
+var app = getApp();
 Page({
-  options:{
+  options: {
     addGlobalClass: true,
   },
   data: {
-
+    avatarUrl: '',
+    nickName:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {//通过openid获取到数据库中的用户信息
+    db.collection('userInfo').where({
+      _openid : app.globalData.openid
+    }).get({
+      success: res => {
+        console.log(res.data)
+        this.setData({
+          avatarUrl: res.data[0].avatarUrl,
+          nickName:res.data[0].userInfo.nickName,
+        })
+        
+      }
+    })
+
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
