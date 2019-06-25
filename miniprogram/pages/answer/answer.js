12e56ae840164f1a141950fb4b34349fed9dc8f9
Jack <l644081641@gmail.com>
2019-06-25 20:49:37 +0800

    统计分数，答题卡构建中

--- miniprogram/pages/answer/answer.js
+++ miniprogram/pages/answer/answer.js
@@ -7,7 +7,6 @@ Page({
     },
     data: {
         choosed: [],
-        btnName: '下一题',
         questions: [],
         tags: 0,
         newArr: [], //随机数数组
@@ -16,8 +15,25 @@ Page({
     beforeQuestion: function() { //改变tags改变题目
         tag = tag - 1;
         if (tag < 0) {
-            console.log("第一题");
+            wx.showToast({
+                title: '已经是第一题！',
+                icon: 'none',
+                duration: 2000
+            })
             tag = 0;
+        } else {
+            this.setData({
+                tags: tag,
+            })
+        }
+
+    },
+
+    nextQuestion: function() {
+        tag = tag + 1;
+        if (tag >= this.data.newArr.length) {
+            console.log("最后一题");
+            tag = this.data.newArr.length-1;
         } else {
             this.setData({
                 tags: tag,
@@ -25,31 +41,40 @@ Page({
         }
     },
 
-    nextQuestion: function() {
-        tag = tag + 1;
-        if (tag >= this.data.newArr.length) {
-            console.log("最后一题");
-            tag = this.data.newArr.length;
-        } else {
-            this.setData({
-                tags: tag,
-            })
-        };
-    },
-
     chooseAnswer: function(res) {
         let mTag = this.data.newArr[this.data.tags];
         let index = res.currentTarget.dataset.index;
         let chooseArr = this.data.questions[mTag].options;
-        let nowChecked = 'questions['+mTag+'].options';//setData改变部分数据
-        if (chooseArr[index].checked) return;//选择当前已经选择的返回
-        chooseArr.forEach(item => {//
+        let nowChecked = 'questions[' + mTag + '].options'; //setData改变部分数据
+        if (chooseArr[index].checked) return; //选择当前已经选择的返回
+        chooseArr.forEach(item => { //遍历选项，将其他选项设置为false（单选）
             item.checked = false
         })
         chooseArr[index].checked = true;
         this.setData({
-            [nowChecked]:chooseArr, 
+            [nowChecked]: chooseArr,
         })
+        if (tag < this.data.newArr.length){
+            this.data.choosed.push(chooseArr[index].value);//将选中的数据保存到数组中去
+        };
+        var that = this;
+        setTimeout(function() { //设置延时后自动跳转到下一题
+            tag = tag + 1;
+            if (tag >= that.data.newArr.length) {
+                console.log("最后一题");
+                tag = that.data.newArr.length-1;
+            } else {
+                that.setData({
+                    tags: tag,
+                })
+            }
+        }, 700)
+        
+        
+    },
+
+    submit:function(){
+        console.log(this.data.choosed);
     },
 
     onLoad: function(options) {
