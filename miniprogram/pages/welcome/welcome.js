fa399ca9acd7ca96cfc87b4c4082e1aab47c3438
Jack <l644081641@gmail.com>
2019-06-27 20:58:26 +0800

    登录界面修改

--- miniprogram/pages/welcome/welcome.js
+++ miniprogram/pages/welcome/welcome.js
@@ -4,6 +4,7 @@ Page({
   data: {
     avatarUrl: '',
     userInfo: {},
+    
   },
   userLogin: function () {
     wx.getSetting({ //调用接口判断是否登录
@@ -45,14 +46,8 @@ Page({
                 } else {
                   console.log('已有记录，不添加');
                 }
-              },
-              fail: res => {
-                console.log(res)
               }
             })
-          },
-          fail: res => {
-            console.log(res)
           }
         })
       },
@@ -60,48 +55,44 @@ Page({
         console.log('接口调用失败')
       }
     })
-
-    // 调用云函数
-
-
-
-
-
-
-
   },
+
   goIndex: function () { //授权完成跳转到首页
     db.collection('userInfo').where({
       _openid: app.globalData.openid,
     }).get({
       success: res => {
+        if (res.data.length == 0) {
+          wx.showToast({
+            title: '信息变更，请重新注册信息！',
+            icon: 'none',
+          })
+          console.log('未找到记录，正在添加');
+          db.collection('userInfo').add({
+            data: {
+              avatarUrl: this.data.avatarUrl,
+              userInfo: this.data.userInfo,
+            }
+          })
+        }
         db.collection('userInfo').doc(res.data[0]._id).get({
           success: res => { //判断数据库中是否有相应openid的注册数据，如果没有则跳转到注册页面
             if ((res.data.name || res.data._class) == undefined) {
               wx.reLaunch({
                 url: '../register/register',
-                success: res => {
-                  console.log(res)
-                }
               })
             } else {
               wx.reLaunch({ //有相关注册数据，跳转到首页
                 url: '../index/index',
-                success: res => {
-                  console.log(res)
-                }
               })
-
             }
           }
         })
       }
     })
-
   },
 
   onLoad: function (options) {
-
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -116,64 +107,6 @@ Page({
         }
       }
     })
-
-
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-
-
-
-
-
-
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
+
 })
