5c89f4c4f45994d68c6b96cd1f253639e6b4654c
Jack <l644081641@gmail.com>
2019-10-05 21:07:32 +0800

    其他

--- miniprogram/pages/comment/comment.js
+++ miniprogram/pages/comment/comment.js
@@ -3,21 +3,22 @@ const db = wx.cloud.database();
 Page({
 
   data: {
-
+    stars: [0, 1, 2, 3, 4],
+    nullsrc: '../../images/null.png',
+    halfsrc: '../../images/half.png',
+    allsrc: '../../images/all.png',
   },
 
-  goBack:function(){
+  goBack: function() {
     wx.navigateBack({
       delta: 1,
     })
   },
-
-
-
-  onLoad: function (options) {
+ 
+  onLoad: function(options) {
 
   },
 
 
-  
+
 })
