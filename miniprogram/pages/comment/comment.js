2fc72aea155f0729b8efa5a05acd6db737e19ae5
Jack <l644081641@gmail.com>
2019-10-06 16:39:05 +0800

    other

--- miniprogram/pages/comment/comment.js
+++ miniprogram/pages/comment/comment.js
@@ -3,22 +3,23 @@ const db = wx.cloud.database();
 Page({
 
   data: {
+    teachers: [],
+    teaInf: {},
     fullStarUrl: '../../images/fullstar.png',
-    nullStarUrl: '../../images/nullstar.png', 
+    nullStarUrl: '../../images/nullstar.png',
     score: 0,
-    scoreArray: [1, 2, 3, 4, 5], 
-    scoreText: ['失望', '勉强', '还行', '不错', '推荐'], 
-    scoreContent: ''
-
+    scoreArray: [1, 2, 3, 4, 5],
+    scoreText: ['1星', '2星', '3星', '4星', '5星'],
+    scoreContent: '',
+    commentContent: '',
   },
   changeScore: function (e) {
-    console.log(e)//控制台触摸事件信息
     var that = this;
     var num = 0;
     var touchX = e.touches[0].pageX;
     var starMinX = 90;
     var starWidth = 20;
-    var starLen =10;
+    var starLen = 10;
     var starMaxX = starWidth * 5 + starLen * 4 + starMinX;
     if (touchX > starMinX && touchX < starMaxX) {
       num = Math.ceil((touchX - starMinX) / (starWidth + starLen));
@@ -36,10 +37,38 @@ Page({
     }
   },
 
-  onLoad: function (options) {
+  submit: function (e) {
+    console.log(e)
+  },
+
+  back: function () {
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
+
+  textareaAInput: function (e) {
+
+
 
   },
 
 
-
+  onLoad: function (e) {
+    db.collection("teacherInfo").get({
+      success: res => {
+        this.setData({
+          teachers: res.data[0].teachers, //拿到关于教师数据
+        })
+        for (let i = 0; i <= this.data.teachers.length; i++) {
+          if (this.data.teachers[i].name == app.globalData.teacher) {
+            this.setData({
+              teaInf: this.data.teachers[i], //筛选出选中的教师数据
+            })
+            console.log(this.data.teaInf)
+          }
+        }
+      },
+    })
+  }
 })
